d0150b854bcc9a1a1bd7c094feb59eb3b963ea58
Mkamalsiddharth <111132531+Mkamalsiddharth@users.noreply.github.com>
2022-08-13 12:47:40 +0530

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,19 +1,30 @@
+#!usr/bin/python3.10.6
+
 import socket
 import threading
 import sys
+
+#Welcome banner
+
 print("-"*50)
 Banner = "Welcome To CyBenSec Port Scanning"
 Sub = "Enter Ip address to scan"
 print(Banner.center(20))
 print(Sub.center(22))
 print("-"*50)
-target = input("Enter Target ip address : ").strip()
 
+#storing user input as userinput
+
+userinput=input(Target ip address : ").strip()
+
+#name resolution error handling
 try:
-    target_ip = socket.gethostbyname(target)
+    target_ip = socket.gethostbyname(userinput)
 except:
     socket.gaierror
     sys.exit()
+
+#defining scan function 
 def scan_port(port):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.settimeout(2)
@@ -21,6 +32,8 @@ def scan_port(port):
     if connection == 0:
         print(f"Port {port} is Open")
         s.close()
+
+#implementing scan funcation with all ports(65536) at a time using threading
 for port in range(65536):
     thread = threading.Thread(target=scan_port, args=(port,))
     thread.start()
